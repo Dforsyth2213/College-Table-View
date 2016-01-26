1ff69196ab9361a21ea447721dfa1f96443d963d
dforsyth <student@dforsyth.local>
2016-01-26 12:44:00 -0600

    Created detail View Controller

--- College Table View/DetailViewController.swift
+++ College Table View/DetailViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var collegeTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var college: College!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collegeTextField.text = college.name
+        stateTextField.text = college.state
+        populationTextField.text = String(college.population)
+        imageView.image = college.image
 
     }
+    @IBAction func onTappedSaveButton(sender: AnyObject) {
+        college.name = collegeTextField.text!
+        college.state = stateTextField.text!
+        college.population = Int(populationTextField.text!)!
+    }
+
 
    
 }
